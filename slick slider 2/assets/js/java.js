d98bdaec9211255dd27253824c9806b860e62d4a
naman-398 <139863654+naman-398@users.noreply.github.com>
2023-08-19 10:05:19 +0530

    Add files via upload

---
+++ slick slider 2/assets/js/java.js
@@ -0,0 +1,11 @@
+$('.slide').slick({
+    slidesToShow: 3,
+    Infinity: true,
+    slidesToScroll: 1,
+    autoplay: false,
+    autoplaySpeed: 200,
+    arrows:true,
+  prevArrow:'.btnnxt',
+  nextArrow:'.btnprev',
+  dots:true,
+});
